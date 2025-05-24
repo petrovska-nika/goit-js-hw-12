9bdd30845e9999d3353b419336be8f7945cb21e3
petrovska-nika <petrovska.nika.dev@gmail.com>
2025-05-25 00:05:04 +0300

    fix: add full image properties to gallery cards

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -11,10 +11,18 @@ export function createGallery(images) {
   const markup = images
     .map(
       image => `
-    <a href="${image.largeImageURL}" class="gallery-item">
-      <img src="${image.webformatURL}" alt="${image.tags}" />
-    </a>
-  `
+      <li class="gallery-item">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p><b>Likes:</b> ${image.likes}</p>
+          <p><b>Views:</b> ${image.views}</p>
+          <p><b>Comments:</b> ${image.comments}</p>
+          <p><b>Downloads:</b> ${image.downloads}</p>
+        </div>
+      </li>
+    `
     )
     .join('');
   galleryContainer.insertAdjacentHTML('beforeend', markup);
